a115b2eb436fdbc6d4007b516c27cc5266330c22
liuxu <liuxu623@gmail.com>
2017-05-18 12:57:41 +0800

    add calico-upgrade role

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-filename="lain-pre-1476939656.tar.gz"
+filename="lain-pre-1495082869.tar.gz"
 if [ ! -f "$filename" ]; then
     wget https://lain.oss-cn-beijing.aliyuncs.com/${filename}
 fi
