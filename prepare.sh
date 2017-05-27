5dfc2a7a77a3bd92fd0ae2f990a0a78637c0b9bf
liuxu <liuxu623@gmail.com>
2017-05-27 18:32:57 +0800

    add calico-upgrade role and update networkd role

--- prepare.sh
+++ prepare.sh
@@ -7,3 +7,7 @@ fi
 pre_dir=playbooks/roles/binary/files/
 [ -d $pre_dir ] || mkdir $pre_dir
 tar -xzvf $filename -C playbooks/roles/binary/files/
+
+wget https://github.com/laincloud/networkd/releases/download/v0.1.20/networkd -P playbooks/roles/networkd/files/
+wget https://github.com/laincloud/calico-upgrade/releases/download/v0.0.1/calico-upgrade -P playbooks/roles/calico-upgrade/files/bin/
+wget https://github.com/projectcalico/calicoctl/releases/download/v1.2.1/calicoctl -P playbooks/roles/calico-upgrade/files/bin/
